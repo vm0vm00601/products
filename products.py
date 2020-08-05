0325d286e9b8c7269ca8eb96b8eff09ab89af0b8
Benson Lin <vm0vm00601@gmail.com>
2020-08-05 12:38:20 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,11 @@
+products = []
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格：')
+	p = []
+	p.append(name)
+	p.append(price)   # p = [name, price] 等於789三行
+	products.append(p)   # products.append([name, price])等於7~10四行
+print(products)
