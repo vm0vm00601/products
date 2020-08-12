0aa3c2055fbf166596eabc2562942a4697749f95
Benson Lin <vm0vm00601@gmail.com>
2020-08-12 11:44:04 +0800

    add header

--- products.py
+++ products.py
@@ -12,3 +12,14 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+
+# 'abc' + '123' = 'abc123'
+# 'abc' * 3 = 'abcabcabc'
+
+with open('products.csv', 'w', encoding='utf-8') as f:   # 'w' 寫入模式
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')  # open只有打開檔案, 有write才會寫入
+# \n 是換行	
+# 離開with架構就會自動close 	
